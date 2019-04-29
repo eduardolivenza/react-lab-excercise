30eb44640629f76ee89b61641e347583877f84bd
Martinez <martie78@emea.roche.com>
2019-04-29 14:57:23 +0200

    unit test + merge

--- front/src/pods/hotel-edit/hotel-edit.vm.ts
+++ front/src/pods/hotel-edit/hotel-edit.vm.ts
@@ -1,15 +1,16 @@
+import { FieldValidationResult } from "lc-form-validation";
 
 export interface HotelEntityVm {
-  id : string;
-  picture : string;
-  name : string;
-  description : string;
-  rating : number;
-  address : string;
-  city : string;
+  id: string;
+  picture: string;
+  name: string;
+  description: string;
+  rating: number;
+  address: string;
+  city: string;
 }
 
-export const createDefaultHotel = ()  : HotelEntityVm => ({
+export const createDefaultHotel = (): HotelEntityVm => ({
   id: '0',
   picture: '',
   name: '',
@@ -18,3 +19,16 @@ export const createDefaultHotel = ()  : HotelEntityVm => ({
   address: '',
   city: '',
 });
+
+export interface HotelFormErrors {
+  name: FieldValidationResult;
+  city: FieldValidationResult;
+  description: FieldValidationResult;
+}
+
+export const createDefaultHotelFormErrors = (): HotelFormErrors => ({
+  name: new FieldValidationResult(),
+  city: new FieldValidationResult(),
+  description: new FieldValidationResult()
+});
+
